2bf8590eb4456dd9f76dc895db7f52d3353a3f2c
Nix3732 <eaz055@mail.ru>
2024-03-09 15:59:33 +0200

    1.0

--- L3.cpp
+++ L3.cpp
@@ -1,67 +1,7 @@
 ﻿#include <iostream>
-
-class str
-{
-    char* _str;
-
-public:
-    str(const char* str) //констурктор
-    {
-        int l = strlen(str);
-        _str = new char[l+1];
-
-        for (int i = 0; i < l; i++)
-        {
-            _str[i] = str[i];
-        }
-        _str[l] = '\0';
-    }
-
-    str(const str& second) //констурктор 2
-    {
-        int l = strlen(second._str);
-        _str = new char[l+1];
-
-        for (int i = 0; i < l; i++)
-        {
-            _str[i] = second._str[i];
-        }
-        _str[l] = '\0';
-    }
-
-    str& operator=(const str& second)
-    {
-        delete[] _str;
-        int l = strlen(second._str);
-        _str = new char[l + 1];
-
-        for (int i = 0; i < l; i++)
-        {
-            _str[i] = second._str[i];
-        }
-
-        _str[l] = '\0';
-
-        return *this;
-    }
-
-    void out() //Вывод
-        {
-            std::cout << _str << std::endl;
-        }
-
-    ~str() //деструктор
-    {
-        delete[] _str;
-    }
-};
-
-
-
-
+#include "string.hpp"
 int main()
 {   
-
     str s1("adsaf");
     str s2("qweqwr");
     s1.out();
---
+++ string.hpp
@@ -0,0 +1,57 @@
+﻿#pragma once
+#include <iostream>
+class str
+{
+    char* _str;
+
+public:
+    str(const char* str) //констурктор
+    {
+        int l = strlen(str);
+        _str = new char[l + 1];
+
+        for (int i = 0; i < l; i++)
+        {
+            _str[i] = str[i];
+        }
+        _str[l] = '\0';
+    }
+
+    str(const str& second) //констурктор 2
+    {
+        int l = strlen(second._str);
+        _str = new char[l + 1];
+
+        for (int i = 0; i < l; i++)
+        {
+            _str[i] = second._str[i];
+        }
+        _str[l] = '\0';
+    }
+
+    str& operator=(const str& second)
+    {
+        delete[] _str;
+        int l = strlen(second._str);
+        _str = new char[l + 1];
+
+        for (int i = 0; i < l; i++)
+        {
+            _str[i] = second._str[i];
+        }
+
+        _str[l] = '\0';
+
+        return *this;
+    }
+
+    void out() //Вывод
+    {
+        std::cout << _str << std::endl;
+    }
+
+    ~str() //деструктор
+    {
+        delete[] _str;
+    }
+};
